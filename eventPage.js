d3bc2c33e3131b2109ca6271102225121c7531db
sprt <hellosprt@gmail.com>
2015-09-09 00:15:14 +0200

    Force enable Chrome's prerendering setting

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,5 @@
+chrome.privacy.network.networkPredictionEnabled.set({value: true});
+
 chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
   chrome.tabs.get(addedTabId, function(addedTab) {
     console.log("Replaced tab", addedTabId, addedTab);
